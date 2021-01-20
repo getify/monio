501bde5ba2c6edca152a21510c97e1916dbd3ea7
Kyle Simpson <getify@gmail.com>
2021-01-20 14:16:42 -0600

    tweaking index exports, exposing monio-util

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 "use strict";
 
-var Just = require("./just.js");
-var Nothing = require("./nothing.js");
-var Maybe = require("./maybe.js");
-var Either = require("./either.js");
-var AsyncEither = require("./async-either.js");
+module.exports.MonioUtil = require("./lib/util.js");
+module.exports.Just = require("./just.js");
+module.exports.Nothing = require("./nothing.js");
+module.exports.Maybe = require("./maybe.js");
+module.exports.Either = require("./either.js");
+module.exports.AsyncEither = require("./async-either.js");
 var IO = require("./io.js");
-var IOHelpers = require("./io-helpers.js");
-var IOEventStream = require("./io-event-stream.js");
-
-module.exports.Just = Just;
-module.exports.Nothing = Nothing;
-module.exports.Maybe = Maybe;
-module.exports.Either = Either;
-module.exports.AsyncEither = AsyncEither;
 module.exports.IO = IO;
-module.exports.Reader = IO;
 module.exports.RIO = IO;
-module.exports.IOHelpers = IOHelpers;
-module.exports.IOEventStream = IOEventStream;
+module.exports.Reader = IO;
+module.exports.IOHelpers = require("./io-helpers.js");
+module.exports.IOEventStream = require("./io-event-stream.js");
